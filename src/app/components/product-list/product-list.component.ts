c94846929753cf48f35e55f3f299743fa792899c
cypher20b <nuel.emma20@gmail.com>
2024-09-06 05:58:55 +0000

    feat: inject model service

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -9,6 +9,7 @@ import { ICart, IProduct } from '../../interfaces/product-item';
 import { ModalComponent } from '../modal/modal.component';
 import { MatDialog } from '@angular/material/dialog';
 import { ApiService } from '../../services/apiservice/api.service';
+import { ModelService } from '../../services/model/model.service';
 @Component({
   selector: 'app-product-list',
   standalone: true,
@@ -23,43 +24,10 @@ import { ApiService } from '../../services/apiservice/api.service';
   styleUrl: './product-list.component.scss'
 })
 export class ProductListComponent {
-  cart$ = new BehaviorSubject<ICart[]>([]);
-  products!:Observable<IProduct[]>
-  constructor(
+  constructor( 
+    public modelService:ModelService,
     public variables: VariablesService,
-    private apiService : ApiService,
-  ) {
-    this.apiService.fetchProducts().subscribe({
-      next:(prod)=>{
-        this.products = of(prod);
-        console.log(this.products);
-      },
-      error:(err)=>console.error(err),
-      complete:()=>console.log('complete')
-    })
-
-    this.cart$.subscribe({
-      next:(prod)=>console.log(prod),
-      error:(err)=>console.error(err),
-      complete:()=>console.log('complete')
-    })
-
-  }
-
-  addToCart(product:IProduct) {
-    let item = {...product, quantity:1}
-    this.cart$.next([...this.cart$.getValue(), item])
-  }
-  isInCart(product: IProduct) {
-    let item = {...product, quantity:1}
-    return of(this.cart$.getValue().some(item=>item.id === product.id))
-  }
-  removeFromCart(productId:number){
-    let cartItems = this.cart$.getValue()
-    cartItems = cartItems.filter(item=>item.id !== productId)
-    this.cart$.next(cartItems)
-  }
-
-
+    public dialog: MatDialog
+  ){}
 }
 
